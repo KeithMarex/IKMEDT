141ea59cc39b2f31f90de6b624825d811224165f
KoennieTime <koenvandermarel@gmail.com>
2021-06-15 17:28:19 +0200

    [Fix] Code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-AFRAME.registerComponent("screw1", {
-  schema: {
-    default: ""
-  },
-  init: function() {
-    var data = this.data;
-    var el = this.el;
-    // var fernando = document.querySelector("#fernando");
-    el.addEventListener("mouseenter", function() {
-      // fernando.setAttribute("scale", "2 2 2");
-      alert('JOEHOE');
-    });
-  }
-});
+const screws = ['screw1', 'screw2', 'screw3', 'screw4', 'screw5', 'screw6', 'screw7', 'screw8', 'screw9'];
+
+for (let screw of screws){
+  AFRAME.registerComponent(screw, {
+    init: function () {
+      var el = this.el;
+      var data = this.data;
+
+      removeScrew(el);
+    }
+  });
+}
+
+removeScrew = (element) => {
+  return element.addEventListener("mouseenter", () => {
+    element.remove();
+  })
+}
