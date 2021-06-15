e1d60ccbc780a1299474c54ade05d011d22b2c50
Ajob Kustra <ajobkustra.p@gmail.com>
2021-06-15 17:40:24 +0200

    Remove this.data

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ for (let i = 1; i <= 9; i++){
   AFRAME.registerComponent(`screw${i}`, {
     init: function () {
       var el = this.el;
-      var data = this.data;
 
       removeScrew(el);
     }
