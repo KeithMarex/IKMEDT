175c3011bc05afba05abdf3fce4934e1ea356bc3
Ajob Kustra <ajobkustra.p@gmail.com>
2021-06-18 16:04:15 +0200

    Progress until now

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-const obj = document.getElementsByClassName('pickup')
+const equippableItems = document.getElementsByClassName('pickup')
+const camera = document.getElementById("camera")
+const heldItem = null
+
+console.log(equippableItems)
+
+for (let i = 0; i < equippableItems.length; i++) {
+  console.log(equippableItems[i])
+  equippableItems[i].addEventListener('click', (event) => {
+    console.log('CLICKED')
+    console.log(event)
+    if (heldItem == null) {
+      
+    }
+  })
+}
 
 for (let i = 1; i <= 9; i++){
   AFRAME.registerComponent(`screw${i}`, {
@@ -12,8 +27,9 @@ for (let i = 1; i <= 9; i++){
 
 removeScrew = (element) => {
   return element.addEventListener("click", () => {
+    console.log('CLICKED')
     // element.remove();
     element.setAttribute('color', 'red')
-    console.log(element);
+    // console.log(element);
   })
 }
