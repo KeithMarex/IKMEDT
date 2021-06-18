c3dfd24943a90f0b8f5dee79b84a3a9694a68ff2
Ajob Kustra <ajobkustra.p@gmail.com>
2021-06-18 17:33:53 +0200

    Pretty dirty functional code for taking screwdrivers and putting them back. Not the worst, could have more abstraction

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
 const equippableItems = document.getElementsByClassName('pickup')
+const screwdriversContainer = document.getElementById('screwdrivers')
 const equippableItemsBackup = equippableItems
 const camera = document.getElementById("camera")
 let heldItem = null
 
 for (let equippableItem of equippableItems) {
-  console.log(equippableItem)
-  equippableItem.addEventListener('click', function(event) {
-    console.log('CLICKED')
-    if (heldItem !== null) {
-      return
-    }
-
-    let copiedNode = event.currentTarget.cloneNode(true)
-    
-    copiedNode.setAttribute('rotation', "65 110 100")
-    copiedNode.setAttribute('position', "1 -1 -5")
-
-    camera.appendChild(copiedNode)
-
-    if (heldItem == null) {
-      heldItem = copiedNode
-    }
-    this.remove()
-  })
+  equippableItem.addEventListener('click', changeScrewdriver)
 }
 
 for (let i = 1; i <= 9; i++){
@@ -42,4 +25,51 @@ removeScrew = (element) => {
     element.setAttribute('color', 'red')
     // console.log(element);
   })
+}
+
+function setHeldItem(item) {
+  heldItem = item
+
+  heldItem.setAttribute('position', "0.55 -0.192 -1.161")
+  heldItem.setAttribute('rotation', "-75.6 21.8 172.2")
+  heldItem.setAttribute('scale', "0.1 0.1 0.1")
+
+  camera.appendChild(heldItem)
+}
+
+function changeScrewdriver(event) {
+  if (heldItem !== null) {
+    let foundBackup = null
+
+    for (let i = 0; i < equippableItemsBackup.length; i++) {
+      let backupId = equippableItemsBackup[i].getAttribute('id')
+      let heldId = heldItem.getAttribute('id')
+
+      if (backupId == heldId)
+        foundBackup = equippableItemsBackup[i]
+    }
+
+    if (foundBackup == null) return
+
+    let clonedBackup = foundBackup.cloneNode(true)
+    clonedBackup.addEventListener('click', changeScrewdriver)
+    screwdriversContainer.appendChild(clonedBackup)
+
+    heldItem.remove()
+    this.remove()
+
+    setHeldItem(event.currentTarget.cloneNode(true))
+
+    return
+  }
+
+  let copiedNode = event.currentTarget.cloneNode(true)
+  
+  setHeldItem(copiedNode)
+
+  if (heldItem == null) {
+    heldItem = copiedNode
+  }
+
+  this.remove()
 }
